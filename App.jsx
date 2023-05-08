d24e3a5e895adcdd1200f0525012fdfbe93669f0
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-05-08 15:50:21 -0300

    primeira parte de projeto concluida

---
+++ App.jsx
@@ -0,0 +1,94 @@
+import React, {useEffect, useState} from 'react';
+import { SafeAreaView, Text, Button, FlatList, View, Image, StyleSheet, ActivityIndicator } from 'react-native';
+
+const App = () => {
+
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    const requestMovies = async () => {
+      setLoading(true);
+      const req = await fetch("https://api.b7web.com.br/cinema/");
+      const json = await req.json();
+
+      if(json){
+        setMovies(json);
+      }
+
+      setLoading(false)
+    }
+    
+    requestMovies();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+
+      {loading &&
+       <View style={styles.loadingArea}>
+        <ActivityIndicator size="large" color="#FFFFFF" />
+        <Text style={styles.loadingText}>Carregando...</Text>
+       </View>
+      }
+
+      {!loading &&
+        <>
+          <Text style={styles.totalMoviesText}>Total de Filmes: {movies.length}</Text>
+          <FlatList 
+            style={styles.list}
+            data={movies}
+            renderItem={({item})=>(
+              <View style={styles.movieItem}>
+                <Image source={{uri: item.avatar}} style={styles.movieImage} resizeMode="contain" />
+                <Text style={styles.movieTitle} >{item.titulo}</Text>
+              </View>
+            )}
+            keyExtractor={item => item.titulo}
+          />
+        </>
+      }
+      
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333'
+  },
+  loadingArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loadingText: {
+    color: '#FFF'
+  },
+  totalMoviesText: {
+    color: '#FFF',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  list: {
+    flex: 1,
+  },
+  movieItem: {
+    marginBottom: 30,
+  },
+  movieImage: {
+    height: 400,
+  },
+  movieTitle: {
+    color: '#FFF',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 5
+  }
+
+})
+
+export default App;
---
+++ README.md
@@ -0,0 +1,12 @@
+##Requisitos
+
+###Node.js versão 14 ou superior
+###npm ou yarn (recomendado)
+###Um emulador ou dispositivo para rodar o aplicativo
+
+##Instalação
+Para instalar as dependências do projeto, execute o seguinte comando na pasta raiz do projeto:
+###yarn install
+ou
+###npm install
+
